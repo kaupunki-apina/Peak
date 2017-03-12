4fdec9df0f6e2a03eb80bf94550ef59e827ac37c
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-12 19:29:48 +0200

    Starting / stopping service onResume / onStop

--- app/src/main/java/fi/salminen/tomy/peak/activities/tracking/TrackingActivity.java
+++ app/src/main/java/fi/salminen/tomy/peak/activities/tracking/TrackingActivity.java
@@ -1,5 +1,6 @@
 package fi.salminen.tomy.peak.activities.tracking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
@@ -7,6 +8,7 @@ import fi.salminen.tomy.peak.R;
 import fi.salminen.tomy.peak.app.PeakApplication;
 import fi.salminen.tomy.peak.core.BaseActivity;
 import fi.salminen.tomy.peak.inject.activity.BaseActivityModule;
+import fi.salminen.tomy.peak.services.bus.BusLocationService;
 
 
 public class TrackingActivity extends BaseActivity<TrackingActivityComponent> {
@@ -15,6 +17,7 @@ public class TrackingActivity extends BaseActivity<TrackingActivityComponent> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
+
     }
 
     @NonNull
@@ -24,6 +27,18 @@ public class TrackingActivity extends BaseActivity<TrackingActivityComponent> {
                 .baseActivityModule(new BaseActivityModule(this))
                 .peakApplicationComponent(PeakApplication.getApplication(this).component())
                 .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startService(new Intent(TrackingActivity.this, BusLocationService.class));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopService(new Intent(TrackingActivity.this, BusLocationService.class));
     }
 
     @Override
