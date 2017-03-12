7331b727840081cfa56325a17c25a5a4b9ef7f01
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-12 15:18:11 +0200

    Custom application class

---
+++ app/src/main/java/fi/salminen/tomy/peak/Peak.java
@@ -0,0 +1,13 @@
+package fi.salminen.tomy.peak;
+
+import android.app.Application;
+
+
+public class Peak extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO Create and store dagger components.
+    }
+}
