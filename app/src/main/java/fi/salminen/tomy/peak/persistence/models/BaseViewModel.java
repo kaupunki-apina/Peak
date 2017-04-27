083ae026132310a4de07f0b27c6d6740522f4be6
Tomy Salminen <tomy.salminen@futurice.com>
2017-04-27 11:22:49 +0300

    Renamed T --> TModel for clarity

--- app/src/main/java/fi/salminen/tomy/peak/persistence/models/BaseViewModel.java
+++ app/src/main/java/fi/salminen/tomy/peak/persistence/models/BaseViewModel.java
@@ -5,11 +5,11 @@ import android.content.Context;
 
 import com.raizlabs.android.dbflow.rx2.structure.BaseRXModel;
 
-public abstract class BaseViewModel<T extends BaseRXModel> {
-    protected T mModel;
+public abstract class BaseViewModel<TModel extends BaseRXModel> {
+    protected TModel mModel;
     protected Context context;
 
-    public BaseViewModel(T model, Context context) {
+    public BaseViewModel(TModel model, Context context) {
         this.mModel = model;
         this.context = context;
     }
