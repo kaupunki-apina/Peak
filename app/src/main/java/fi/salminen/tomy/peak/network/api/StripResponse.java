a3dc76178c42168d6d9622427f362b594254ef92
Tomy Salminen <tomy.salminen@futurice.com>
2017-04-28 07:57:25 +0300

    Redid StripResponse

--- app/src/main/java/fi/salminen/tomy/peak/network/api/StripResponse.java
+++ app/src/main/java/fi/salminen/tomy/peak/network/api/StripResponse.java
@@ -1,12 +1,15 @@
 package fi.salminen.tomy.peak.network.api;
 
 
+import android.support.annotation.NonNull;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
@@ -15,23 +18,38 @@ import okhttp3.ResponseBody;
  * Extracts the payload from API response.
  */
 public class StripResponse implements Interceptor {
+    private final String CONTENT_TYPE = "Content-Type";
+    private JsonParser mParser = new JsonParser();
+
     @Override
     public Response intercept(Interceptor.Chain chain) throws IOException {
         Response response = chain.proceed(chain.request());
-        JsonElement payload = extractPayload(response.body().string());
-        return payload != null
-                ? strippedResponse(payload.toString(), response)
-                : response;
+        String responseBody = response.body().string();
+        String payload = stripExcess(responseBody);
+        return repackage(response, payload);
     }
 
-    private Response strippedResponse(String payloadStr, Response response) throws IOException {
+    @NonNull
+    private Response repackage(@NonNull Response response, @NonNull String payload) {
         return response.newBuilder()
-                .body(ResponseBody.create(response.body().contentType(), payloadStr))
+                .body(ResponseBody.create(getMediaType(response), payload))
                 .build();
     }
 
-    private JsonElement extractPayload(String responseBody) throws IOException {
-        JsonParser parser = new JsonParser();
-        return parser.parse(responseBody).getAsJsonObject().get("body");
+    private MediaType getMediaType(@NonNull Response response) {
+        return MediaType.parse(response.headers().get(CONTENT_TYPE));
+    }
+
+    private String stripExcess(String responseBody) {
+        JsonElement jsonBody = mParser.parse(responseBody);
+        if (jsonBody.isJsonObject()) {
+            JsonElement jsonPayload = jsonBody.getAsJsonObject().get("body");
+
+            if (jsonPayload != null && jsonPayload.isJsonArray()) {
+                return jsonPayload.toString();
+            }
+        }
+
+        return responseBody;
     }
 }
